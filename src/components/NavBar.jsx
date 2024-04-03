55c88a2fe5a79bbeefad565395260b378995dd3d
heritierbagumire <heritierbagumire367@gmail.com>
2024-04-03 18:07:13 +0200

    new changes made on fbars

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -14,54 +14,57 @@ const NavBar = () => {
     {
       id: 2,
       link: "about",
-      title: "Home",
+      title: "About Us",
     },
     {
       id: 3,
       link: "portfolio",
-      title: "Home",
+      title: "Services",
     },
     {
       id: 4,
       link: "experience",
-      title: "Home",
+      title: "experience",
     },
     {
       id: 5,
       link: "contact",
-      title: "Home",
+      title: "contact",
     },
   ];
 
   return (
     <div className="flex flex-col py-2 justify-between items-center w-full h-auto px-10 text-black bg-white fixed">
       <div className="border-b border-green-600 w-[110%]">
-        <h1 className="text-5xl font-signature md:ml-8 my-2 text-green-600">Calm Craft</h1>
+        <h1 className="text-5xl font-signature md:ml-8 my-2 text-green-600">
+          Calm Craft
+        </h1>
       </div>
-
-      <ul className="hidden md:flex md:border-2">
-        {links.map(({ id, link, title }) => (
-          <li
-            key={id}
-            className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200"
-          >
-            <Link to={link} smooth duration={500}>
-              {title}
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <div className="-mr-[60%]">
+        <ul className="hidden md:flex  md:border-2">
+          {links.map(({ id, link, title }) => (
+            <li
+              key={id}
+              className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200"
+            >
+              <Link to={link} smooth duration={500}>
+                {title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
 
       <div
         onClick={() => setNav(!nav)}
         className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
       >
-        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+        {nav ? <FaTimes size={30} /> : <FaBars size={30} className="-mt-10 ml-80" />}
       </div>
 
       {nav && (
         <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
-          {links.map(({ id, link }) => (
+          {links.map(({ id, link, title }) => (
             <li
               key={id}
               className="px-4 cursor-pointer capitalize py-6 text-4xl"
@@ -72,7 +75,7 @@ const NavBar = () => {
                 smooth
                 duration={500}
               >
-                {link}
+                {title}
               </Link>
             </li>
           ))}
