fd40dce45d90fa628d9d288cf1c29db6424f1c80
heritierbagumire <heritierbagumire367@gmail.com>
2024-04-03 22:35:59 +0200

    we are pushing the home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import HeroImage from "../assets/heroImage.jpg";
+import HeroImage from "../assets/calm.jpg";
+import Dfd from "../assets/davido.jpg";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import { Link } from "react-scroll";
 
@@ -7,40 +8,42 @@ const Home = () => {
   return (
     <div
       name="home"
-      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
+      className="md:max-w-screen-2xl md:px-2 lg:px-5 h-auto w-full bg-white text-green-700 mt-6 md:overflow-hidden"
     >
-      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
-        <div className="flex flex-col justify-center h-full">
-          <h2 className="text-4xl sm:text-7xl font-bold text-white">
-            I'm a Full Stack Developer
-          </h2>
-          <p className="text-gray-500 py-4 max-w-md">
-            I have 3 years of experience building and desgining software.
-            Currently, I love to work on web application using technologies like
-            React, Tailwind, Next JS and GraphQL.
-          </p>
-
+      <div className=" mx-2 flex flex-col items-center justify-between h-auto md:flex-row">
+        <div className="flex flex-col justify-center h-full md:w-1/2">
           <div>
-            <Link
-              to="portfolio"
-              smooth
-              duration={500}
-              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
-            >
-              Portfolio
-              <span className="group-hover:rotate-90 duration-300">
-                <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
-              </span>
-            </Link>
+            <h2 className=" text-3xl md:text-6xl lg:text-7xl font-medium mt- text-green-800 ">
+              Calm <br /> Craft
+            </h2>
+            <p className="font-medium text-sm mt-4 md:text-auto">
+              Make a Connection
+            </p>
+          </div>
+          <div className="mt-[60%]">
+            <p className="text-green-600 text-[14px] py-4 pb-2 text-start max-w-">
+              Our Support Group was created as a community for individuals going
+              through similar difficulties. We believe that this safe space,
+              where people of all backgrounds can share with and listen to
+              others, can make a huge difference. Get in touch today.
+            </p>
+            <button className="bg-green-800 text-white text-[12px] px-4 my-10 py-2 rounded-2xl">
+              Learn More
+            </button>
           </div>
         </div>
-
-        <div>
-          <img
-            src={HeroImage}
-            alt="my profile"
-            className="rounded-2xl mt-20 mx-auto w-2/3 md:w-full"
-          />
+        <div className="flex flex-col justify-center h-full md:w-1/2 border-2 bg-background">
+          <div></div>
+          <div className="mt-54">
+            <div className="z-0">
+              <img src={Dfd} alt="fda" className="w-full h-[730px]" />
+            </div>
+            <div className="-inset-10 lg:-inset-40 z-40 relative">
+              <div className="absolute -top-[540px] md:-top-96 right-58">
+                <img src={HeroImage} alt="fda" className="w-48 h-[230px]" />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
