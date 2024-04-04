4aa176ddee92619f22e7ec7fe16eedab67bc17e0
heritierbagumire <heritierbagumire367@gmail.com>
2024-04-04 13:57:23 +0200

    new changes to about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,13 +9,14 @@ const About = () => {
         <div className="max-w-6xl mx-auto px-4 sm:px-6">
           <div className="py-12 md:py-20 border-t mt-10 border-gray-800">
             {/* Section header */}
-            <div className="max-w-7xl mx-auto text-start pb-12 md:pb-16">
-              <div className="inline-flex text-sm font-semibold py-1 px-3 m-2 text-green-600 bg-green-200 rounded-full mb-4">
-                <p className="text-start">
+            <div className="max-w-7xl mx-auto text-start pb-8 md:pb-16">
+              <div className="inline-flex  text-sm font-semibold py-1 px-3 m-2 text-green-800 rounded-full mb-4">
+                <p className="text-start text-4xl">
                 About Us
                 </p>
-              </div>
-              <h1 className="h2 mb-4">One product, unlimited solutions</h1>
+                </div>
+              
+              <h1 className="h2 mb-4 ml-6 text-green-800 font-medium">Who we are</h1>
               <p className="text-xl text-gray-400">
                 Excepteur sint occaecat cupidatat non proident, sunt in culpa
                 qui officia deserunt mollit laborum — semper quis lectus nulla.
@@ -23,7 +24,7 @@ const About = () => {
             </div>
 
             {/* Items */}
-            <div className="grid gap-20 grid-flow-row-dense">
+            <div className="grid gap-20 grid-flow-row border-2 border-black">
               {/* 1st item */}
               <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
                 {/* Image */}
