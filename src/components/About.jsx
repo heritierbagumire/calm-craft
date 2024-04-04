3ed1840d066355137153d8eaab2ac40ada22ed84
heritierbagumire <heritierbagumire367@gmail.com>
2024-04-04 13:04:00 +0200

    new files added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,41 +1,230 @@
 import React from "react";
-
+import FeatImage01 from "../assets/features-03-image-01.png";
+import FeatImage02 from "../assets/features-03-image-03.png";
+import FeatImage03 from "../assets/hero-image-01.jpg";
 const About = () => {
   return (
-    <div
-      name="about"
-      className="w-full h-screen bg-white text-green-800"
-    >
-      <div className="max-w-screen-lg p-0 mx-10 flex flex-col justify-center w-full h-full">
-        <div className="pb-8">
-          <p className="text-4xl font-bold inline border-gray-500">
-            About Us
-          </p>
-          <p className="text-xl mt-20">
-            Hello there! 👋 I'm Heritier BAGUMIRE, a passionate software developer
-            with three years of hands-on experience in crafting innovative and
-            efficient solutions. My journey in the world of coding has led me to
-            excel in a diverse range of technologies, with a particular focus on
-            Vue, Angular, Node.js, React, and React Native.
-          </p>
+    <div name="about" className="w-full h-auto bg-white text-green-800">
+      <section>
+        <div className="max-w-6xl mx-auto px-4 sm:px-6">
+          <div className="py-12 md:py-20 border-t mt-10 border-gray-800">
+            {/* Section header */}
+            <div className="max-w-7xl mx-auto text-start pb-12 md:pb-16">
+              <div className="inline-flex text-sm font-semibold py-1 px-3 m-2 text-green-600 bg-green-200 rounded-full mb-4">
+                <p className="text-start">
+                About Us
+                </p>
+              </div>
+              <h1 className="h2 mb-4">One product, unlimited solutions</h1>
+              <p className="text-xl text-gray-400">
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa
+                qui officia deserunt mollit laborum — semper quis lectus nulla.
+              </p>
+            </div>
+
+            {/* Items */}
+            <div className="grid gap-20 grid-flow-row-dense">
+              {/* 1st item */}
+              <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+                {/* Image */}
+                <div
+                  className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1"
+                  data-aos="fade-up"
+                >
+                  <img
+                    className="max-w-full mx-auto md:max-w-none h-auto"
+                    src={FeatImage01}
+                    width={540}
+                    height={405}
+                    alt="Features 01"
+                  />
+                </div>
+                {/* Content */}
+                <div
+                  className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6"
+                  data-aos="fade-right"
+                >
+                  <div className="md:pr-4 lg:pr-12 xl:pr-16">
+                    <div className="font-architects-daughter text-xl text-purple-600 mb-2">
+                      More speed. Less spend
+                    </div>
+                    <h3 className="h3 mb-3">Keep projects on schedule</h3>
+                    <p className="text-xl text-gray-400 mb-4">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua.
+                    </p>
+                    <ul className="text-lg text-gray-400 -mb-2">
+                      <li className="flex items-center mb-2">
+                        <svg
+                          className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                          viewBox="0 0 12 12"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                        </svg>
+                        <span>Duis aute irure dolor in reprehenderit</span>
+                      </li>
+                      <li className="flex items-center mb-2">
+                        <svg
+                          className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                          viewBox="0 0 12 12"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                        </svg>
+                        <span>Excepteur sint occaecat</span>
+                      </li>
+                      <li className="flex items-center">
+                        <svg
+                          className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                          viewBox="0 0 12 12"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                        </svg>
+                        <span>Amet consectetur adipiscing elit</span>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+
+              {/* 2nd item */}
+              <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+                {/* Image */}
+                <div
+                  className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 rtl"
+                  data-aos="fade-up"
+                >
+                  <img
+                    className="max-w-full mx-auto md:max-w-none h-auto"
+                    src={FeatImage02}
+                    width={540}
+                    height={405}
+                    alt="Features 02"
+                  />
+                </div>
+                {/* Content */}
+                <div
+                  className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6"
+                  data-aos="fade-left"
+                >
+                  <div className="md:pl-4 lg:pl-12 xl:pl-16">
+                    <div className="font-architects-daughter text-xl text-purple-600 mb-2">
+                      More speed. Less spend
+                    </div>
+                    <h3 className="h3 mb-3">Keep projects on schedule</h3>
+                    <p className="text-xl text-gray-400 mb-4">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua.
+                    </p>
+                    <ul className="text-lg text-gray-400 -mb-2">
+                      <li className="flex items-center mb-2">
+                        <svg
+                          className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                          viewBox="0 0 12 12"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                        </svg>
+                        <span>Duis aute irure dolor in reprehenderit</span>
+                      </li>
+                      <li className="flex items-center mb-2">
+                        <svg
+                          className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                          viewBox="0 0 12 12"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                        </svg>
+                        <span>Excepteur sint occaecat</span>
+                      </li>
+                      <li className="flex items-center">
+                        <svg
+                          className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                          viewBox="0 0 12 12"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                        </svg>
+                        <span>Amet consectetur adipiscing elit</span>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+
+              {/* 3rd item */}
+              <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+                {/* Image */}
+                <div
+                  className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1"
+                  data-aos="fade-up"
+                >
+                  <img
+                    className="max-w-full mx-auto md:max-w-none h-auto"
+                    src={FeatImage03}
+                    width={540}
+                    height={405}
+                    alt="Features 03"
+                  />
+                </div>
+                {/* Content */}
+                <div
+                  className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6"
+                  data-aos="fade-right"
+                >
+                  <div className="md:pr-4 lg:pr-12 xl:pr-16">
+                    <div className="font-architects-daughter text-xl text-purple-600 mb-2">
+                      More speed. Less spend
+                    </div>
+                    <h3 className="h3 mb-3">Keep projects on schedule</h3>
+                    <p className="text-xl text-gray-400 mb-4">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua.
+                    </p>
+                    <ul className="text-lg text-gray-400 -mb-2">
+                      <li className="flex items-center mb-2">
+                        <svg
+                          className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                          viewBox="0 0 12 12"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                        </svg>
+                        <span>Duis aute irure dolor in reprehenderit</span>
+                      </li>
+                      <li className="flex items-center mb-2">
+                        <svg
+                          className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                          viewBox="0 0 12 12"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                        </svg>
+                        <span>Excepteur sint occaecat</span>
+                      </li>
+                      <li className="flex items-center">
+                        <svg
+                          className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                          viewBox="0 0 12 12"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                        </svg>
+                        <span>Amet consectetur adipiscing elit</span>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <p className="text-4xl font-bold inline border-b-4 border-gray-500 my-8">
-          My journey
-        </p>
-
-        <p className="text-xl mt-20">
-          My fascination with software development ignited during my college
-          years when I undertook a project that involved automating a complex
-          data analysis process. Struggling with the intricacies of the task, I
-          stumbled upon the world of coding as a potential solution. As I delved
-          deeper into writing lines of code and witnessing the immediate impact
-          it had on data manipulation, a newfound passion was born.
-        </p>
-
-        <br />
-
-        <p className="text-xl"></p>
-      </div>
+      </section>
     </div>
   );
 };
