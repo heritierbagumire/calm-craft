50728df15eb97c9f27ab9c6e4e29e90cc87d8e14
heritierbagumire <heritierbagumire367@gmail.com>
2024-04-04 13:56:47 +0200

    new change to app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import Navbar from './components/NavBar'
 import Home from './components/Home'
 import About from './components/About';
-import Portfolio from './components/Portfolio';
 import Experience from './components/Experience';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 function App() {
   return (
     <div>
     <Navbar />
     <Home />
     <About />
-    <Portfolio /> 
     <Experience /> 
     <Contact />
+    <Footer />
     </div>
   );
 }
