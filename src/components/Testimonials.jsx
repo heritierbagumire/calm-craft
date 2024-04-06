deb3cfa15304142e152dd860a67dec6a4bd53ebe
heritierbagumire <heritierbagumire367@gmail.com>
2024-04-06 14:14:07 +0200

    testimonials added

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,149 +1,98 @@
 
 
-import TestimonialImage01 from "@/public/images/testimonial-01.jpg";
-import TestimonialImage02 from "@/public/images/testimonial-02.jpg";
-import TestimonialImage03 from "@/public/images/testimonial-03.jpg";
+import TestimonialImage01 from "../assets/davido.jpg";
+import TestimonialImage02 from "../assets/features-03-image-03.png";
+
 
 export default function Testimonials() {
   return (
     <section>
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
-        <div className="py-12 md:py-20 border-t border-gray-800">
-          {/* Section header */}
-          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-            <h2 className="h2 mb-4">Don't take our word for it</h2>
-            <p className="text-xl text-gray-400">
-              Vitae aliquet nec ullamcorper sit amet risus nullam eget felis
-              semper quis lectus nulla at volutpat diam ut venenatis tellus—in
-              ornare.
-            </p>
-          </div>
-
-          {/* Testimonials */}
-          <div className="max-w-sm mx-auto grid gap-8 lg:grid-cols-3 lg:gap-6 items-start lg:max-w-none">
-            {/* 1st testimonial */}
-            <div
-              className="flex flex-col h-full p-6 bg-gray-800"
-              data-aos="fade-up"
-            >
-              <div>
-                <div className="relative inline-flex flex-col mb-4">
+        <div>
+          <div className="w-auto h-auto pb-0 overflow-auto z-0 inset-0">
+            <div className="container mx-auto px-2 md:px-20 lg:px-36 flex flex-col items-center justify-center">
+              <div className=" max-w-8xl border-2  rounded-lg md:h-64 w-full mt-20 flex flex-col md:flex-row-reverse  md:gap-x-2 transition duration-150 ease-in-out">
+                <div className="w-96 h-18 md:w-2/5 md:h-full rounded-lg px-4 pt-4">
                   <img
-                    className="rounded-full"
+                    className=" w-48 h-48 mx-24 md:mx-2 lg:mx-5 md:w-48 md:h-48 object-cover rounded-full"
+                    alt="profile"
                     src={TestimonialImage01}
-                    width={48}
-                    height={48}
-                    alt="Testimonial 01"
                   />
-                  <svg
-                    className="absolute top-0 right-0 -mr-3 w-6 h-5 fill-current text-purple-600"
-                    viewBox="0 0 24 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M0 13.517c0-2.346.611-4.774 1.833-7.283C3.056 3.726 4.733 1.648 6.865 0L11 2.696C9.726 4.393 8.777 6.109 8.152 7.844c-.624 1.735-.936 3.589-.936 5.56v4.644H0v-4.531zm13 0c0-2.346.611-4.774 1.833-7.283 1.223-2.508 2.9-4.586 5.032-6.234L24 2.696c-1.274 1.697-2.223 3.413-2.848 5.148-.624 1.735-.936 3.589-.936 5.56v4.644H13v-4.531z" />
-                  </svg>
+                </div>
+                <div className="w-full h-full md:w-3/5 lg:w-4/5 md:h-48 text-xs md:text-default-800 px-4 pt-10 block ">
+                  <p className="text-center text-lg font-semibold"> Hofa Doe</p>
+                  <p className="text-center text-pretty font-normal">
+                    Today, we gather to honour the continued sacrifice of
+                    survivors and remember all those we lost during the Genocide
+                    against the Tutsi, which really means, people are just being
+                    Today, we gather to honour the continued sacrifice of
+                    
+                  </p>
                 </div>
               </div>
-              <blockquote className="text-lg text-gray-400 grow">
-                — Open PRO lets me quickly get the insights I care about so that
-                I can focus on my productive work. I've had Open PRO for about
-                24 hours now and I honestly don't know how I functioned without
-                it before.
-              </blockquote>
-              <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
-                <cite className="text-gray-200 not-italic">Anastasia Dan</cite>{" "}
-                -{" "}
-                <a
-                  className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out"
-                  href="#0"
-                >
-                  UX Board
-                </a>
-              </div>
-            </div>
-
-            {/* 2nd testimonial */}
-            <div
-              className="flex flex-col h-full p-6 bg-gray-800"
-              data-aos="fade-up"
-              data-aos-delay="200"
-            >
-              <div>
-                <div className="relative inline-flex flex-col mb-4">
+              <div className=" max-w-6xl shadow-3-strong rounded-lg md:h-80 w-full mt-20 flex flex-col  md:flex-row-reverse  md:gap-x-2 hover:contrast-50">
+                <div className="w-full h-18 md:w-4/5 md:h-full rounded-lg px-4 py-4 ">
                   <img
-                    className="rounded-full"
+                    className="w-full h-full object-cover rounded-md"
+                    alt="profile"
                     src={TestimonialImage02}
-                    width={48}
-                    height={48}
-                    alt="Testimonial 02"
                   />
-                  <svg
-                    className="absolute top-0 right-0 -mr-3 w-6 h-5 fill-current text-purple-600"
-                    viewBox="0 0 24 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M0 13.517c0-2.346.611-4.774 1.833-7.283C3.056 3.726 4.733 1.648 6.865 0L11 2.696C9.726 4.393 8.777 6.109 8.152 7.844c-.624 1.735-.936 3.589-.936 5.56v4.644H0v-4.531zm13 0c0-2.346.611-4.774 1.833-7.283 1.223-2.508 2.9-4.586 5.032-6.234L24 2.696c-1.274 1.697-2.223 3.413-2.848 5.148-.624 1.735-.936 3.589-.936 5.56v4.644H13v-4.531z" />
-                  </svg>
+                </div>
+                <div className="w-full h-fill md:w-3/5 md:h-48 text-xs md:text-default-800 px-4 py-2 block ">
+                  <p className="text-start py-2 font-mulish font-thin">
+                    Today, we gather to honour the continued sacrifice of
+                    survivors and remember all those we lost during the Genocide
+                    against the Tutsi, which really means, people are just being
+                    targeted and killed for who they were. And nobody here, or
+                    anywhere in this world, chooses what to be in that sense.
+                    Nobody chose to be what tribe, race, ethnic group. There are
+                    many things we choose to be. You can choose your religion,
+                    but you don’t choose to be the person to be targeted. In
+                    fact, even those who targeted them, have not chosen to
+                    belong to that ethnic group or things like that.
+                  </p>
+                  <div className="flex flex-row-reverse justify-between items-center mt-7">
+                    <div>
+                      <div className="flex space-x-2 inset-4 text-black cursor-pointer">
+                        <p className=" text-default-800">Published by: </p>
+                        <p className="text-custom-blue">Manzi Cedric</p>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
-              <blockquote className="text-lg text-gray-400 grow">
-                — Open PRO lets me quickly get the insights I care about so that
-                I can focus on my productive work. I've had Open PRO for about
-                24 hours now and I honestly don't know how I functioned without
-                it before.
-              </blockquote>
-              <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
-                <cite className="text-gray-200 not-italic">Anastasia Dan</cite>{" "}
-                -{" "}
-                <a
-                  className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out"
-                  href="#0"
-                >
-                  UX Board
-                </a>
-              </div>
-            </div>
-
-            {/* 3rd testimonial */}
-            <div
-              className="flex flex-col h-full p-6 bg-gray-800"
-              data-aos="fade-up"
-              data-aos-delay="400"
-            >
-              <div>
-                <div className="relative inline-flex flex-col mb-4">
+              <div className=" max-w-6xl shadow-3-strong rounded-lg md:h-80 w-full mt-20 flex flex-col  md:flex-row-reverse  md:gap-x-2 hover:contrast-50">
+                <div className="w-full h-18 md:w-4/5 md:h-full rounded-lg px-4 py-4 ">
                   <img
-                    className="rounded-full"
-                    src={TestimonialImage03}
-                    width={48}
-                    height={48}
-                    alt="Testimonial 03"
+                    className="w-full h-full object-cover rounded-md"
+                    alt="profile"
+                    src={TestimonialImage02}
                   />
-                  <svg
-                    className="absolute top-0 right-0 -mr-3 w-6 h-5 fill-current text-purple-600"
-                    viewBox="0 0 24 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M0 13.517c0-2.346.611-4.774 1.833-7.283C3.056 3.726 4.733 1.648 6.865 0L11 2.696C9.726 4.393 8.777 6.109 8.152 7.844c-.624 1.735-.936 3.589-.936 5.56v4.644H0v-4.531zm13 0c0-2.346.611-4.774 1.833-7.283 1.223-2.508 2.9-4.586 5.032-6.234L24 2.696c-1.274 1.697-2.223 3.413-2.848 5.148-.624 1.735-.936 3.589-.936 5.56v4.644H13v-4.531z" />
-                  </svg>
+                </div>
+                <div className="w-full h-fill md:w-3/5 md:h-48 text-xs md:text-default-800 px-4 py-2 block ">
+                  <p className="text-start py-2 font-mulish font-thin">
+                    Today, we gather to honour the continued sacrifice of
+                    survivors and remember all those we lost during the Genocide
+                    against the Tutsi, which really means, people are just being
+                    targeted and killed for who they were. And nobody here, or
+                    anywhere in this world, chooses what to be in that sense.
+                    Nobody chose to be what tribe, race, ethnic group. There are
+                    many things we choose to be. You can choose your religion,
+                    but you don’t choose to be the person to be targeted. In
+                    fact, even those who targeted them, have not chosen to
+                    belong to that ethnic group or things like that.
+                  </p>
+                  <div className="flex flex-row-reverse justify-between items-center mt-7">
+                    <div>
+                      <div className="flex space-x-2 inset-4 text-black cursor-pointer">
+                        <p className=" text-default-800">Published by: </p>
+                        <p className="text-custom-blue">Manzi Cedric</p>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
-              <blockquote className="text-lg text-gray-400 grow">
-                — Open PRO lets me quickly get the insights I care about so that
-                I can focus on my productive work. I've had Open PRO for about
-                24 hours now and I honestly don't know how I functioned without
-                it before.
-              </blockquote>
-              <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
-                <cite className="text-gray-200 not-italic">Anastasia Dan</cite>{" "}
-                -{" "}
-                <a
-                  className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out"
-                  href="#0"
-                >
-                  UX Board
-                </a>
-              </div>
+              <div></div>
             </div>
           </div>
         </div>
