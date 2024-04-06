ec653ae0b4b01948830629534808ee8b8b09e8d5
heritierbagumire <heritierbagumire367@gmail.com>
2024-04-07 01:25:55 +0200

    testimonials added

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -26,70 +26,43 @@ export default function Testimonials() {
                     survivors and remember all those we lost during the Genocide
                     against the Tutsi, which really means, people are just being
                     Today, we gather to honour the continued sacrifice of
-                    
                   </p>
                 </div>
               </div>
-              <div className=" max-w-6xl shadow-3-strong rounded-lg md:h-80 w-full mt-20 flex flex-col  md:flex-row-reverse  md:gap-x-2 hover:contrast-50">
-                <div className="w-full h-18 md:w-4/5 md:h-full rounded-lg px-4 py-4 ">
+              <div className=" max-w-8xl border-2  rounded-lg md:h-64 w-full mt-20 flex flex-col md:flex-row  md:gap-x-2 transition duration-150 ease-in-out">
+                <div className="w-96 h-18 md:w-2/5 md:h-full rounded-lg px-4 pt-4">
                   <img
-                    className="w-full h-full object-cover rounded-md"
+                    className=" w-48 h-48 mx-24 md:mx-2 lg:mx-5 md:w-48 md:h-48 object-cover rounded-full"
                     alt="profile"
-                    src={TestimonialImage02}
+                    src={TestimonialImage01}
                   />
                 </div>
-                <div className="w-full h-fill md:w-3/5 md:h-48 text-xs md:text-default-800 px-4 py-2 block ">
-                  <p className="text-start py-2 font-mulish font-thin">
+                <div className="w-full h-full md:w-3/5 lg:w-4/5 md:h-48 text-xs md:text-default-800 px-4 pt-10 block ">
+                  <p className="text-center text-lg font-semibold"> Hofa Doe</p>
+                  <p className="text-center text-pretty font-normal">
                     Today, we gather to honour the continued sacrifice of
                     survivors and remember all those we lost during the Genocide
                     against the Tutsi, which really means, people are just being
-                    targeted and killed for who they were. And nobody here, or
-                    anywhere in this world, chooses what to be in that sense.
-                    Nobody chose to be what tribe, race, ethnic group. There are
-                    many things we choose to be. You can choose your religion,
-                    but you don’t choose to be the person to be targeted. In
-                    fact, even those who targeted them, have not chosen to
-                    belong to that ethnic group or things like that.
+                    Today, we gather to honour the continued sacrifice of
                   </p>
-                  <div className="flex flex-row-reverse justify-between items-center mt-7">
-                    <div>
-                      <div className="flex space-x-2 inset-4 text-black cursor-pointer">
-                        <p className=" text-default-800">Published by: </p>
-                        <p className="text-custom-blue">Manzi Cedric</p>
-                      </div>
-                    </div>
-                  </div>
                 </div>
               </div>
-              <div className=" max-w-6xl shadow-3-strong rounded-lg md:h-80 w-full mt-20 flex flex-col  md:flex-row-reverse  md:gap-x-2 hover:contrast-50">
-                <div className="w-full h-18 md:w-4/5 md:h-full rounded-lg px-4 py-4 ">
+              <div className=" max-w-8xl border-2 mb-10 rounded-lg md:h-64 w-full mt-20 flex flex-col md:flex-row-reverse  md:gap-x-2 transition duration-150 ease-in-out">
+                <div className="w-96 h-18 md:w-2/5 md:h-full rounded-lg px-4 pt-4">
                   <img
-                    className="w-full h-full object-cover rounded-md"
+                    className=" w-48 h-48 mx-24 md:mx-2 lg:mx-5 md:w-48 md:h-48 object-cover rounded-full"
                     alt="profile"
-                    src={TestimonialImage02}
+                    src={TestimonialImage01}
                   />
                 </div>
-                <div className="w-full h-fill md:w-3/5 md:h-48 text-xs md:text-default-800 px-4 py-2 block ">
-                  <p className="text-start py-2 font-mulish font-thin">
+                <div className="w-full h-full md:w-3/5 lg:w-4/5 md:h-48 text-xs md:text-default-800 px-4 pt-10 block ">
+                  <p className="text-center text-lg font-semibold"> Hofa Doe</p>
+                  <p className="text-center text-pretty font-normal">
                     Today, we gather to honour the continued sacrifice of
                     survivors and remember all those we lost during the Genocide
                     against the Tutsi, which really means, people are just being
-                    targeted and killed for who they were. And nobody here, or
-                    anywhere in this world, chooses what to be in that sense.
-                    Nobody chose to be what tribe, race, ethnic group. There are
-                    many things we choose to be. You can choose your religion,
-                    but you don’t choose to be the person to be targeted. In
-                    fact, even those who targeted them, have not chosen to
-                    belong to that ethnic group or things like that.
+                    Today, we gather to honour the continued sacrifice of
                   </p>
-                  <div className="flex flex-row-reverse justify-between items-center mt-7">
-                    <div>
-                      <div className="flex space-x-2 inset-4 text-black cursor-pointer">
-                        <p className=" text-default-800">Published by: </p>
-                        <p className="text-custom-blue">Manzi Cedric</p>
-                      </div>
-                    </div>
-                  </div>
                 </div>
               </div>
               <div></div>
