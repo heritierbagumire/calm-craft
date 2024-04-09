4bd85900ecd67105cdfa1d75f5fcbe9ca0f456a3
heritierbagumire <heritierbagumire367@gmail.com>
2024-04-09 02:11:21 +0200

    index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import "./index.css";
 import App from "./App";
+
+AOS.init({
+  duration: 800, // Duration of animation (in milliseconds)
+  once: true, // Whether animation should only happen once while scrolling down
+  easing: "ease-out", // Easing options
+  // Add other options here as needed
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
