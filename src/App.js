878c3766205e0cc8d4f942552bab243a7006f253
heritierbagumire <heritierbagumire367@gmail.com>
2024-04-09 23:58:27 +0200

    Index changed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import Navbar from './components/NavBar'
 import Home from './components/Home'
 import About from './components/About';
-import Contact from './components/Contact';
 import Footer from './components/Footer';
 import SocialLinks from './components/SocialLinks';
-import services from './components/services';
 import Testimonials from './components/Testimonials';
 function App() {
   return (
